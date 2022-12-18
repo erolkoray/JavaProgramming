0fe2ebafbd98d3e4a34966bcbb0a4d2893548ac1
Koray <koray_4404@hotmail.com>
2022-12-18 21:15:47 +0000

    package day38_Inheritence - method overriding

--- src/day38_Inheritence/carTask/BMW.java
+++ src/day38_Inheritence/carTask/BMW.java
@@ -1,5 +1,16 @@
 package day38_Inheritence.carTask;
 
-public class BMW {
+public class BMW extends Car{
 
+    public BMW(String model, int year, String colour, double price, int miles) {
+        super("BMW", model, year, colour, price, miles);
+    }
+
+    public void breakdown(){
+        System.out.println(brand+" "+model+" has broken down");
+    }
+
+    public void racing(){
+        System.out.println(brand+" "+model+" is racing");
+    }
 }
--- src/day38_Inheritence/carTask/Tesla.java
+++ src/day38_Inheritence/carTask/Tesla.java
@@ -1,6 +1,12 @@
 package day38_Inheritence.carTask;
 
-public class Tesla {
+public class Tesla extends Car{
 
+    public Tesla(String model, int year, String colour, double price, int miles) {
+        super("Tesla", model, year, colour, price, miles);
+    }
 
+    public void autoPilot(){
+        System.out.println(brand+" "+model+" is on autopilot");
+    }
 }
