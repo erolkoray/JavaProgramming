2efbb5e32f01a9ab7917e09c6f8c0bb228b6094e
Koray <koray_4404@hotmail.com>
2022-12-24 18:30:43 +0000

    package day40 EncapsulationReview and ProtectedAccessModifier

---
+++ src/day40_FinalKeyword/ProtectedAccessModifier.java
@@ -0,0 +1,17 @@
+package day40_FinalKeyword;
+
+public class ProtectedAccessModifier {
+
+
+    static String name1 = "cydeo"; // default access modifier
+    protected static String name2 = "Woodenspoon"; // protected access modifier
+
+    static void method() {
+
+    }
+
+    protected static void method2() {
+
+    }
+
+}
