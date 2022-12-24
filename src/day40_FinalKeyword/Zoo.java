32d6edb4f0916d3bf629d2eeb2639c30299b16f4
Koray <koray_4404@hotmail.com>
2022-12-24 22:07:50 +0000

    package day40 updated zoo class

--- src/day40_FinalKeyword/Zoo.java
+++ src/day40_FinalKeyword/Zoo.java
@@ -5,6 +5,7 @@ public class Zoo {
     public static void main(String[] args) {
 
         Dog dog = new Dog("Max", "Husky", 'M', "White", "Large", 4);
+        Dog dog1 = new Dog("Koray", "Lab", 'F', "black", "Small", 10);
 
         System.out.println(dog.getBreed());
         System.out.println(dog.getGender());
@@ -12,6 +13,20 @@ public class Zoo {
         dog.drink();
         dog.bark();
         dog.eat();
+
+        System.out.println(dog1.getBreed());
+        System.out.println(dog1.getGender());
+
+        dog1.eat();
+        dog1.drink();
+        dog1.bark();
+
+        // you can't change the FINAL variables in the object, eg. breed/colour/gender by setting a different option. So dog1's breed can't be changed by using dog1.setBreed(); but age can be  dog1.setAge(); because Breed is a FINAL method but Age isn't.
+
+       //dog1.setBreed(); - this gives compile error because it was set as a FINAL method
+
+
+
 
 
 
