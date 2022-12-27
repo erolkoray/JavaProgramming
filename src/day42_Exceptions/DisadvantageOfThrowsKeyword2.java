700aa866095d38a6ee6a8a704705f5d21a3b675c
Koray <koray_4404@hotmail.com>
2022-12-27 23:09:23 +0000

    package day42 - updated

--- src/day42_Exceptions/DisadvantageOfThrowsKeyword2.java
+++ src/day42_Exceptions/DisadvantageOfThrowsKeyword2.java
@@ -5,6 +5,22 @@ import java.io.FileNotFoundException;
 
 public class DisadvantageOfThrowsKeyword2 {
 
+    public static void main(String[] args) throws InterruptedException{
+
+        method1();
+        method2();
+        method3();
+
+        String str = null;
+        try {
+            System.out.println(str.charAt(0));
+        }catch (RuntimeException e){
+            e.printStackTrace();
+        }
+
+
+    }
+
     public static void method1() throws InterruptedException { // THROWS ONLY FIXES THE EXCEPTION ISSUE WITHIN THE METHOD IT IS USED
        // new FileInputStream("K");
         Thread.sleep(200);
@@ -14,7 +30,7 @@ public class DisadvantageOfThrowsKeyword2 {
   //      method1(); // Method1() will have the exception issue because THROWS keyword is not inheritable to other methods/classes etc.
     }
 
-    public static void method3() throws FileNotFoundException, InterruptedException { // ADD MULTIPLE EXCEPTIONS IF NEEDED
+    public static void method3() throws InterruptedException { // ADD MULTIPLE EXCEPTIONS IF NEEDED
         method1();
         Thread.sleep(2000);
     }
