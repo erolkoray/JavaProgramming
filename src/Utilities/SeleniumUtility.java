ef38dd69c653588fb194a3246dc11b12815f360c
Koray <koray_4404@hotmail.com>
2022-12-27 22:41:17 +0000

    package Utilities - added SeleniumUtility class and updated the class with Sleep method

---
+++ src/Utilities/SeleniumUtility.java
@@ -0,0 +1,12 @@
+package Utilities;
+
+public class SeleniumUtility {
+
+    public static void sleep(double seconds){
+        try {
+            Thread.sleep((long)(seconds*1000));
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
